64bdb56e16a02206e2386930b041faf525d9c0d8
AniketDas2007 <81820190+AniketDas2007@users.noreply.github.com>
2021-05-11 11:49:01 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,20 @@
+class Dustbin{
+    constructor(x,y,width,height){
+       var options ={
+        isStatic: true,
+        'restitution':0,
+        'friction':1,
+        'density':0.1
+       }
+       this.body = Bodies.rectangle(x,y,width,height,options);
+       this.width = width;
+       this.height = height;
+       World.add(world,this.body);
+    };
+    display(){
+        var pos =this.body.position;
+        rectMode(CENTER);
+        fill(255, 255, 255, 0.5);
+        rect(pos.x, pos.y, this.width, this.height);
+      }
+}
